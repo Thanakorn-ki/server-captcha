6972b3599dd366e85b4db5e6dfa166848113066d
Thanakorn <thanakorn.ki@linecorp.com>
2019-01-06 14:12:27 +0700

    feat: add mongo when insert answer

--- go.mod
+++ go.mod
@@ -1,1 +1,15 @@
 module github.com/salapao2136/server-captcha
+
+require (
+	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/go-mgo/mgo v0.0.0-20180705113738-7446a0344b78
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/salapao2136/captcha v0.0.0-20190105085456-a685718ea212
+	github.com/ugorji/go/codec v0.0.0-20181209151446-772ced7fd4c2 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,80 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	mgo "github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/salapao2136/captcha"
+)
 
 func main() {
-	r := gin.Default()
+	uri := fmt.Sprintf("mongodb://root:root@127.0.0.1:27017")
+
+	fmt.Println("Program Start...")
+	session, err := mgo.Dial(uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+	collection := session.DB("workshop").C("captcha")
+
+	r := gin.New()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/captcha", func(c *gin.Context) {
+		answer, captcha := generateCaptcha()
+
+		_, err := collection.Upsert(
+			bson.M{
+				"captcha": captcha,
+			},
+			bson.M{
+				"captcha": captcha,
+				"answer":  answer,
+			})
+
+		if err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+		}
+
+		c.JSON(200, gin.H{
+			"message": captcha,
+			"answer":  answer,
+		})
+	})
+
+	r.POST("/captcha", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "jwt",
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func generateCaptcha() (int, string) {
+	rand.Seed(time.Now().UnixNano())
+	first := rand.Intn(8) + 1
+	second := rand.Intn(8) + 1
+	ops := rand.Intn(2) + 1
+	var sum = 0
+	if ops == 1 {
+		sum = first + second
+	} else if ops == 2 {
+		sum = first - second
+	} else if ops == 3 {
+		sum = first * second
+	}
+	captcha := captcha.Captcha(rand.Intn(1)+1, first, ops, second)
+	return sum, captcha
 }
